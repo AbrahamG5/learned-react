d7d773724158aaa2c5a8c23b59f0917a8189d73f
AbrahamG5 <abrahamgg055@gmail.com>
2025-10-19 23:49:18 -0600

    End of section

--- src/carrito/ItemCounter.tsx
+++ src/carrito/ItemCounter.tsx
@@ -1,6 +1,8 @@
-import type { CSSProperties } from "react";
+import { useState } from "react";
 
-const sectionStyle: CSSProperties = {
+import "./ItemCounter.css";
+
+/* const sectionStyle: CSSProperties = {
   borderRadius: 10,
   backgroundColor: "#f79292ff",
   display: "flex",
@@ -25,29 +27,56 @@ const itemSpanStyle: CSSProperties = {
   padding: 2,
   textAlign: "center",
   borderRadius: 5,
-};
+}; */
 
 interface Props {
   name: string;
   itemQuantity: number | undefined;
 }
 
-export const ItemCounter = ({ name, itemQuantity }: Props) => {
-  const handleClick = () => {
-    console.log(`Click en ${name}`);
+export const ItemCounter = ({ name, itemQuantity = 1 }: Props) => {
+  const [count, setCount] = useState(itemQuantity);
+
+  const handleAdd = () => {
+    if (count === 1) {
+      return;
+    }
+    return setCount(count + 1);
   };
+  const handleRest = () => {
+    if (count === 1) {
+      return;
+    }
+    return setCount(count - 1);
+  };
+
+  /* const handleClick = () => {
+    console.log(`Click en ${name}`);
+  }; */
   return (
-    <section style={sectionStyle}>
-      <span style={itemSpanStyle}>{name}</span>
+    <section className="sectionStyle">
+      <span className="itemSpanStyle">{name}</span>
       <button
-        onClick={() => {
+        /* onMouseEnter={() => {
+        console.log(`Mouse enter ${name}`);
+      }} */
+        /* onClick={() => {
           handleClick();
-        }}
+        }} */
+        onClick={handleAdd}
       >
         +1
       </button>
-      <span style={counterSpanStyle}>{itemQuantity}</span>
-      <button>-1</button>
+      <span className="counterSpanStyle">{count}</span>
+      <button onClick={handleRest}>-1</button>
+      <span
+        className="itemTestStyle"
+        style={{
+          color: count === 1 ? "red" : "black",
+        }}
+      >
+        Tests
+      </span>
     </section>
   );
 };
