6d73e02a8219c9ea37c1cc675dcd5d783ee6249a
AbrahamG5 <abrahamgg055@gmail.com>
2025-10-24 16:17:43 -0600

    Revert "avance"
    This reverts commit 8be28ebb20e767bdf362f0266a79af3cc11687d7.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { FirstStepsApp } from "./FirstStepsApp";
-/* import { MyAwesomeApp } from "./MyAwesomeApp"; */
-import './index.css'
+import { MyAwesomeApp } from "./MyAwesomeApp";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
