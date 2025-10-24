8be28ebb20e767bdf362f0266a79af3cc11687d7
AbrahamG5 <abrahamgg055@gmail.com>
2025-10-24 16:11:50 -0600

    avance

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { FirstStepsApp } from "./FirstStepsApp";
-import { MyAwesomeApp } from "./MyAwesomeApp";
+/* import { MyAwesomeApp } from "./MyAwesomeApp"; */
+import './index.css'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
